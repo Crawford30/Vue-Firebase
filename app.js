3327df711bb438b0eaa1717e596b8754a704e2a9
joel <joelcrawford30@gmail.com>
2021-06-14 13:55:42 +0300

    data attribute binding

--- app.js
+++ app.js
@@ -8,12 +8,13 @@ const app = Vue.createApp({
   //creates a function and returns an object in that function so that it can be accessible from our template
   data(){
       return{
+          url: "www.google.com",
 
         showBooks: true,
        books: [
-           { title: 'The Final Empire', author: 'Joel Crawford' },
-           { title: 'The River Between', author: 'John Crawford' },
-           { title: 'Big Nerd Ranch', author: 'JJ Crawford' },
+           { title: 'The Final Empire', author: 'Joel Crawford' , img: 'assets/1.jpg'},
+           { title: 'The River Between', author: 'John Crawford' , img: 'assets/2.jpg'},
+           { title: 'Big Nerd Ranch', author: 'JJ Crawford' , img: 'assets/3.jpg'},
        ]
 
       }
